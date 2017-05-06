527b1897cf99ff39928b0c5dd2992d0df04439e2
Trent Willis <trentmwillis@gmail.com>
2017-05-06 14:07:42 -0700

    Add a start/stop button

--- src/ui/components/glimmer-of-life/component.ts
+++ src/ui/components/glimmer-of-life/component.ts
@@ -4,17 +4,19 @@ import World from '../../../utils/world';
 const n = 100;
 
 export default class GlimmerOfLife extends Component {
+  @tracked running = false;
   @tracked world = new World(n);
-
-  didInsertElement() {
-    this.nextGeneration();
-  }
 
   didUpdate() {
     this.nextGeneration();
   }
 
   nextGeneration() {
+    if (!this.running) { return; }
     requestAnimationFrame(() => this.world = this.world.tick());
+  }
+
+  toggle() {
+    this.running = !this.running;
   }
 }
