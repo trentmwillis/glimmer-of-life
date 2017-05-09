552d0e3afe6a325e2e36196fb8847fcfd3c4def1
Trent Willis <trentmwillis@gmail.com>
2017-05-08 20:56:59 -0700

    Change implementation to use a single array instead of a 2D array

--- src/utils/world.ts
+++ src/utils/world.ts
@@ -1,7 +1,7 @@
 import Citizen from './citizen';
 
 export default class World {
-  citizens: Array<Array<Citizen>>;
+  citizens: Array<Citizen>;
   size: number;
 
   constructor(size: number, previousGeneration?: World) {
@@ -19,13 +19,13 @@ export default class World {
 
   populateNewWorld() {
     const size = this.size;
-    const citizens = this.citizens = new Array(size);
+    const citizens: Array<Citizen> = this.citizens = new Array(size);
 
     for (let i = 0; i < size; i++) {
-      const row = citizens[i] = new Array(size);
+      const offset = i * size;
 
       for (let j = 0; j < size; j++) {
-        row[j] = {
+        citizens[offset + j] = {
           alive: Math.random() < 0.2 ? true : false
         };
       }
@@ -35,13 +35,13 @@ export default class World {
   populateNextGeneration(previousGeneration: World) {
     const size = this.size;
 
-    const citizens = this.citizens = new Array(size);
+    const citizens: Array<Citizen> = this.citizens = new Array(size);
 
     for (let i = 0; i < size; i++) {
-      const row = citizens[i] = new Array(size);
+      const offset = i * size;
 
       for (let j = 0; j < size; j++) {
-        const previousGenerationCitizen = previousGeneration.citizens[i][j];
+        const previousGenerationCitizen: Citizen = previousGeneration.citizens[offset + j];
         const neighbors = previousGeneration.findNeighbors(i, j);
 
         let alive = previousGenerationCitizen.alive;
@@ -63,7 +63,7 @@ export default class World {
           alive = true;
         }
 
-        row[j] = { alive };
+        citizens[offset + j] = { alive };
       }
     }
   }
@@ -72,25 +72,22 @@ export default class World {
     const citizens = this.citizens;
     const max = this.size - 1;
 
-    const upIndex = i - 1 < 0 ? max : i - 1;
-    const downIndex = i + 1 > max ? 0 : i + 1;
-
-    const up = citizens[upIndex];
-    const center = citizens[i];
-    const down = citizens[downIndex];
+    const up = (i - 1 < 0 ? max : i - 1) * this.size;
+    const down = (i + 1 > max ? 0 : i + 1) * this.size;
+    const center = i * this.size;
 
     const left = j - 1 < 0 ? max : j - 1;
     const right = j + 1 > max ? 0 : j + 1;
 
     return [
-      up[j],
-      up[left],
-      up[right],
-      center[left],
-      center[right],
-      down[j],
-      down[left],
-      down[right]
+      citizens[up + j],
+      citizens[up + left],
+      citizens[up + right],
+      citizens[center + left],
+      citizens[center + right],
+      citizens[down + j],
+      citizens[down + left],
+      citizens[down + right]
     ];
   }
 
